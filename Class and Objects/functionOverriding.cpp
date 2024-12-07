eebe49c6dc1bc7440c004b69e183ae49d6c755b7
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-07 11:59:12 +0600

    added function overriding

---
+++ Class and Objects/functionOverriding.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Parent
+{
+public:
+    void getInfo()
+    {
+        cout << "parent class\n";
+    }
+};
+
+class Child : public Parent
+{
+public:
+    void getInfo()
+    {
+        cout << "child class\n";
+    }
+};
+
+int main()
+{
+    Child c1;
+    c1.getInfo();
+    return 0;
+}
