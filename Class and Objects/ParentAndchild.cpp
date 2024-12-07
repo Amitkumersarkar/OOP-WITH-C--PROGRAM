bb16f10aca97f2797a8cf8f657c115c5ccc0154c
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-07 09:55:17 +0600

    added parent and child class inheritance

---
+++ Class and Objects/ParentAndchild.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Person
+{
+public:
+    string name;
+    int age;
+    // Person(string name, int age)
+    // {
+    //     this->name = name;
+    //     this->age = age;
+    // }
+
+    // non parameterized constructor
+    Person()
+    {
+        cout << "Parent Constructor..!\n";
+    }
+};
+
+// declared inheritance here
+class Student : public Person
+{
+public:
+    // name, age,id
+    int id;
+
+    Student()
+    {
+        cout << "Child Constructor..!\n";
+    }
+
+    // getInfo print all information of parent and child class
+    void getInfo()
+    {
+        cout << "Student Name : " << name << endl;
+        cout << "Student Age : " << age << endl;
+        cout << "Student ID : " << id << endl;
+    }
+};
+
+int main()
+{
+    // here create an objects
+    Student s1;
+    s1.name = "Amit Sarkar";
+    s1.age = 22;
+    s1.id = 283;
+    // call getInfo for s1
+    s1.getInfo();
+    return 0;
+}
