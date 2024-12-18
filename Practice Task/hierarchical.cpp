046a88db5aff3c475aa1f3cb23227351dea0b278
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-19 02:18:43 +0600

    added hierarchical inheritance

---
+++ Practice Task/hierarchical.cpp
@@ -0,0 +1,43 @@
+// hierarchical inheritance
+
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void display()
+    {
+        cout << "inside the display" << endl;
+    }
+};
+
+class B : public A
+{
+public:
+    void display1()
+    {
+        cout << "inside the display1" << endl;
+    }
+};
+
+class C : public A
+{
+public:
+    void display2()
+    {
+        cout << "inside the display2" << endl;
+    }
+};
+
+int main()
+{
+    A obj;
+    obj.display();
+
+    B obj1;
+    obj1.display1();
+
+    C obj2;
+    obj2.display2();
+    return 0;
+}
